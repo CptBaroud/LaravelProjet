69d264b147206998f90afbb9e682ccbdd68aec8c
arcimed <38589361+arcimed@users.noreply.github.com>
2019-01-16 13:25:02 +0100

    test julien

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Projet web groupe 2
+                    Projet web groupe 2.
                 </div>
 
                 <div class="links">
