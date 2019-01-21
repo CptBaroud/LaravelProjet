84682a4f206ebb4fdd939420125073df64af62f5
arcimed <38589361+arcimed@users.noreply.github.com>
2019-01-21 15:29:22 +0100

    update edit shop + tri

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,18 @@ Route::get('/activities/create', ['as'=>'activitiesCreate', 'uses'=>'PostsContro
 Route::get('/activities', ['as'=>'activitiesIndex', 'uses'=>'ActivitiesController@index']);
 
 
-Route::get('/shop', 'ShopController@index');
+Route::get('/shop',array('as' => 'shop', 'uses' => 'ShopController@index'));
+
 Route::post('/shop/create', array('as' => 'Items_create', 'uses' => 'ShopController@CreateItems'));
 Route::get('/shop/create', array('as' => 'Items_form', 'uses' => 'ShopController@Itemform'));
+
 Route::get('/shop/delete/{id}', array('as' => 'Itemsdelete', 'uses' => 'ShopController@Delete'));
+
+Route::get('/shop/edit/{id}', array('as' => 'shop_Edit', 'uses' => 'shopController@Edit'));
+Route::post('/shop/update/{id}', array('as' => 'shop_update', 'uses' => 'shopController@Update'));
+Route::get('/shop/achat/{id}', array('as' => 'shop_achat', 'uses' => 'shopController@Achat'));
+Route::get('/shop/category/{id}', array('as' => 'shop_category', 'uses' => 'shopController@category'));
+
 
 Route::get('/register', 'RegistrationController@index');
 Route::post('/register', 'RegistrationController@processing');
